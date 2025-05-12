0b36c512203a529185ebc6f49bcb040d2a9f1e3e
Jiyaaaa21 <chandilajiya81@gmail.com>
2025-05-12 20:05:27 +0530

    Expense class added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print("Hello Jiya")
+import datetime
 
+class Expense:
+    def __init__(self, amount, category, description):
+        self.amount = amount
+        self.category = category
+        self.description = description
+        self.date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # This line saves the current date and timew in a clean, readable format
+
+    def __str__(self):  # this make it easy to print the expense in presentable format
+        return f"{self.date} | ₹{self.amount} | {self.category} | {self.description}" 
+    
 
